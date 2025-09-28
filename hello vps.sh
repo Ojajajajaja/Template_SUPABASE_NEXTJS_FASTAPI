9daf2330ddf117efac34871edb09201e136fbbbd
Ojajajajaja <degenoja@gmail.com>
2025-09-28 12:49:35 +0200

    hello vps improved

--- hello vps.sh
+++ hello vps.sh
@@ -5,5 +5,11 @@ chmod +x .setup/scripts/*.sh
 chmod +x .setup/scripts/*/*.sh
 
 echo "================"
-echo "You can now start to setup your project by running make setup-user"
+echo ""
+echo "You can now start to setup your project by running :"
+echo ""
+echo "1) make deps"
+echo "2) make setup-user"
+echo "3) make setup-prod"
+echo ""
 echo "Don't forget to configure your domain's DNS before running make setup-prod"
