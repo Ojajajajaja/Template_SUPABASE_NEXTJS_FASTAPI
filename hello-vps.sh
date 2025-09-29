0ffe7a98b73f9ff61098833f91169f131fd32ea2
Ojajajajaja <degenoja@gmail.com>
2025-09-29 08:22:38 +0200

    improving helloscript

--- hello-vps.sh
+++ hello-vps.sh
@@ -1,16 +1,17 @@
 echo "Hello!"
 apt install make
 
+cp .setup/.env.config.example .setup/.env.config
 chmod +x .setup/scripts/*.sh
 chmod +x .setup/scripts/*/*.sh
+make deps
 
 echo "================"
 echo ""
 echo "You can now start to setup your project by running :"
 echo ""
-echo "1) make deps"
-echo "2) create your own .env.config file by copying .env.config.example in .setup/"
-echo "2) make user"
-echo "3) make setup-prod"
+echo "1) Edit .setup/.env.config"
+echo "2) Use command: make user"
+echo "3) Use command: make setup-prod"
 echo ""
 echo "Don't forget to configure your domain's DNS before running make setup-prod"
